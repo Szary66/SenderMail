eb0172ce9bccb96de6a81f83c5b4c15fcf5e31d0
Lukasz Mlynarski <Lukasz Mlynarski>
2017-01-07 16:29:32 +0100

    Load csv, send mails by the html template, save logs in txt

--- MailSender/Form1.Designer.cs
+++ MailSender/Form1.Designer.cs
@@ -23,12 +23,120 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.LoadCSV = new System.Windows.Forms.Button();
+            this.ListAllMails = new System.Windows.Forms.ListView();
+            this.AddMail = new System.Windows.Forms.Button();
+            this.AllMails = new System.Windows.Forms.Label();
+            this.SendedMails = new System.Windows.Forms.Label();
+            this.SendMail = new System.Windows.Forms.Button();
+            this.ListSendedMails = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // LoadCSV
+            // 
+            this.LoadCSV.Location = new System.Drawing.Point(12, 296);
+            this.LoadCSV.Name = "LoadCSV";
+            this.LoadCSV.Size = new System.Drawing.Size(75, 23);
+            this.LoadCSV.TabIndex = 0;
+            this.LoadCSV.Text = "Load";
+            this.LoadCSV.UseVisualStyleBackColor = true;
+            this.LoadCSV.Click += new System.EventHandler(this.LoadCSV_Click);
+            // 
+            // ListAllMails
+            // 
+            this.ListAllMails.FullRowSelect = true;
+            this.ListAllMails.GridLines = true;
+            this.ListAllMails.HideSelection = false;
+            this.ListAllMails.LabelEdit = true;
+            this.ListAllMails.Location = new System.Drawing.Point(12, 35);
+            this.ListAllMails.MinimumSize = new System.Drawing.Size(152, 238);
+            this.ListAllMails.Name = "ListAllMails";
+            this.ListAllMails.Size = new System.Drawing.Size(173, 238);
+            this.ListAllMails.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.ListAllMails.TabIndex = 1;
+            this.ListAllMails.UseCompatibleStateImageBehavior = false;
+            this.ListAllMails.View = System.Windows.Forms.View.List;
+            // 
+            // AddMail
+            // 
+            this.AddMail.Location = new System.Drawing.Point(93, 296);
+            this.AddMail.Name = "AddMail";
+            this.AddMail.Size = new System.Drawing.Size(75, 23);
+            this.AddMail.TabIndex = 3;
+            this.AddMail.Text = "Add";
+            this.AddMail.UseVisualStyleBackColor = true;
+            // 
+            // AllMails
+            // 
+            this.AllMails.AutoSize = true;
+            this.AllMails.Location = new System.Drawing.Point(64, 10);
+            this.AllMails.Name = "AllMails";
+            this.AllMails.Size = new System.Drawing.Size(44, 13);
+            this.AllMails.TabIndex = 4;
+            this.AllMails.Text = "All mails";
+            // 
+            // SendedMails
+            // 
+            this.SendedMails.AutoSize = true;
+            this.SendedMails.Location = new System.Drawing.Point(206, 10);
+            this.SendedMails.Name = "SendedMails";
+            this.SendedMails.Size = new System.Drawing.Size(70, 13);
+            this.SendedMails.TabIndex = 5;
+            this.SendedMails.Text = "Sended mails";
+            // 
+            // SendMail
+            // 
+            this.SendMail.Location = new System.Drawing.Point(289, 296);
+            this.SendMail.Name = "SendMail";
+            this.SendMail.Size = new System.Drawing.Size(75, 23);
+            this.SendMail.TabIndex = 6;
+            this.SendMail.Text = "Send";
+            this.SendMail.UseVisualStyleBackColor = true;
+            this.SendMail.Click += new System.EventHandler(this.SendMail_Click);
+            // 
+            // ListSendedMails
+            // 
+            this.ListSendedMails.FullRowSelect = true;
+            this.ListSendedMails.GridLines = true;
+            this.ListSendedMails.HideSelection = false;
+            this.ListSendedMails.LabelEdit = true;
+            this.ListSendedMails.Location = new System.Drawing.Point(191, 35);
+            this.ListSendedMails.MinimumSize = new System.Drawing.Size(152, 238);
+            this.ListSendedMails.Name = "ListSendedMails";
+            this.ListSendedMails.Size = new System.Drawing.Size(173, 238);
+            this.ListSendedMails.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.ListSendedMails.TabIndex = 7;
+            this.ListSendedMails.UseCompatibleStateImageBehavior = false;
+            this.ListSendedMails.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 349);
+            this.Controls.Add(this.ListSendedMails);
+            this.Controls.Add(this.SendMail);
+            this.Controls.Add(this.SendedMails);
+            this.Controls.Add(this.AllMails);
+            this.Controls.Add(this.AddMail);
+            this.Controls.Add(this.ListAllMails);
+            this.Controls.Add(this.LoadCSV);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LoadCSV;
+        private System.Windows.Forms.ListView ListAllMails;
+        private System.Windows.Forms.Button AddMail;
+        private System.Windows.Forms.Label AllMails;
+        private System.Windows.Forms.Label SendedMails;
+        private System.Windows.Forms.Button SendMail;
+        private System.Windows.Forms.ListView ListSendedMails;
     }
 }
 
--- MailSender/Form1.cs
+++ MailSender/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +14,84 @@ namespace MailSender {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            ReadCSV();
+            ReadLogs();
+        }
+
+        void SaveLogs() {
+            try {
+                using (StreamWriter outputFile = new StreamWriter("Logs.txt")) {
+                    outputFile.NewLine = "blabla";
+                    foreach (ListViewItem item in ListSendedMails.Items)
+                        outputFile.WriteLine(item.SubItems[0].Text);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void ReadLogs() {
+            try {   // Open the text file using a stream reader.
+                ListSendedMails.Clear();
+                using (StreamReader sr = new StreamReader("Logs.txt")) {
+                    // Read the stream to a string, and write the string to the console.
+                    while (!sr.EndOfStream) {
+                        ListViewItem line = new ListViewItem(sr.ReadLine());
+                        ListSendedMails.Items.Add(line);
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void ReadCSV() {
+            try {   // Open the text file using a stream reader.
+                ListAllMails.Clear();
+                using (StreamReader sr = new StreamReader("emails.csv")) {
+                    // Read the stream to a string, and write the string to the console.
+                    while (!sr.EndOfStream) {
+                        ListViewItem line = new ListViewItem(sr.ReadLine());
+                        ListAllMails.Items.Add(line);
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void LoadCSV_Click(object sender, EventArgs e) {
+            ReadCSV();
+        }
+        
+        private void SendMail_Click(object sender, EventArgs e) {
+            try {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("mailforath123@gmail.com");
+
+                mail.Subject = "Test Mail";
+                mail.IsBodyHtml = true;
+                mail.Body = "<html><h1>Hello</h1><br /><p>Najlepsze życzenia z okazji świąt</p></html>";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("mailforath123@gmail.com", "lukasz123");
+                SmtpServer.EnableSsl = true;
+                
+                foreach (ListViewItem item in ListAllMails.Items) {
+                    mail.To.Add(item.SubItems[0].Text);
+                    SmtpServer.Send(mail);
+                    ListSendedMails.Items.Add(item.SubItems[0].Text);
+
+                }
+                SaveLogs();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
+
